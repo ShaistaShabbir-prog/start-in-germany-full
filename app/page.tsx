c22d1b9dbf648b5333bdf84e7c35c90ce5fe0e61
Shaista Shabbir <125836662+ShaistaShabbir-prog@users.noreply.github.com>
2026-06-13 23:24:23 +0200

    feat: import RecommendationWidget in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import RecommendationWidget from "@/components/RecommendationWidget";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
